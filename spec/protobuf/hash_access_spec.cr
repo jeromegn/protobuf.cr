fe221a4cbf88294921109a63363f724015163a7a
maiha <maiha@wota.jp>
2017-09-25 05:25:11 +0900

    added `Protobuf::Message#[](key)`

---
+++ spec/protobuf/hash_access_spec.cr
@@ -0,0 +1,27 @@
+require "../spec_helper"
+
+require "../fixtures/test.pb"
+
+describe Protobuf::Message do
+  describe "#[key]" do
+    context "when the key is a member" do
+      it "acts as #key" do
+        File.open("#{__DIR__}/../fixtures/test.data.encoded") do |io|
+          test = Test.from_protobuf(io)
+          test["f1"].should eq("dsfadsafsaf")
+        end
+      end
+    end
+
+    context "when the key is not a member" do
+      it "raises a runtime error" do
+        File.open("#{__DIR__}/../fixtures/test.data.encoded") do |io|
+          test = Test.from_protobuf(io)
+          expect_raises(Protobuf::Error, /Field not found/) do
+            test["XX"]
+          end
+        end
+      end
+    end
+  end
+end
