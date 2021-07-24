93f3afcff6627d3a2abf0d6b2b815a658f8500f4
moe <moe@busyloop.net>
2021-07-24 10:33:21 +0200

    Restrict return type of oneof-by-name getter
    The return-type of `myobj.myoneof_value` is now restricted
    to the types of the possible oneof-values. Previously it would
    include the types of *all* message-fields (even those that
    are not part of the queried oneof).

--- src/protobuf/message.cr
+++ src/protobuf/message.cr
@@ -58,11 +58,16 @@ module Protobuf
       end
 
       def {{name.id}}_value
-        self[{{ONEOFS[index].id}}.not_nil!]
+        {% for tag, field in FIELDS %}
+          {% if field[:oneof_index] == index %}
+          return @{{field[:name].id}} if self[{{ONEOFS[index].id}}.not_nil!] == {{ field[:name] }}
+          {% end %}
+        {% end %}
+        nil
       end
 
       def {{name.id}}_value!
-        @{{name.id}}_value.not_nil!
+        {{name.id}}_value.not_nil!
       end
     end
 
