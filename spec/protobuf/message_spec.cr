b3f36ce889253a7b5769944c5007137c0db63aa7
Martyn Loughran <martyn@mloughran.com>
2022-01-27 10:20:06 +0000

    Duplicate proto2 message specs to test proto3 encoding/decoding
    Note that this has uncovered a bug with repeated proto3 enums, which is currently commented out

--- spec/protobuf/message_spec.cr
+++ spec/protobuf/message_spec.cr
@@ -61,4 +61,59 @@ describe Protobuf::Message do
       test4.to_protobuf.empty?.should be_true
     end
   end
+
+  describe "proto3" do
+    it "decodes" do
+      File.open("#{__DIR__}/../fixtures/test.data.proto3-encoded") do |io|
+        test = TestMessagesV3::Test3.from_protobuf(io)
+
+        test.f1.should eq("dsfadsafsaf")
+        test.f2.should eq(234)
+
+        test.fa.should eq([2342134, 2342135, 2342136])
+
+        test.fb.should eq([-2342134, -2342135, -2342136])
+
+        test.fc.should eq [4, 7, -12, 4, 7, -3, 4, 7, 0]
+
+        test.pairs.should eq([
+          TestMessagesV3::Pair.new(key: "sdfff", value: "q\"qq\\q\n"),
+          TestMessagesV3::Pair.new(key: "   sdfff2  тест ", value: "q\tqq<>q2&\u{1}"),
+        ])
+
+        test.bbbb.should eq(Slice[0u8, 1u8, 2u8, 255u8, 254u8, 253u8])
+
+        test.uint32.should eq(4294967295)
+        test.uint64.should eq(18446744073709551615)
+        test.sint32.should eq(-2147483648)
+        test.sint64.should eq(-9223372036854775808)
+        test.bool.should eq true
+
+        # TODO: Encoding & decoding repeated proto3 enums is currently broken
+        # test.enum.should eq [TestMessagesV3::SomeEnum::YES, TestMessagesV3::SomeEnum::NO, TestMessagesV3::SomeEnum::NEVER]
+
+        test.fixed64.should eq 4294967296
+        test.sfixed64.should eq 2147483648
+
+        test.double.should eq 20.64
+
+        test.fixed32.should eq 4294967295
+        test.sfixed32.should eq 2147483647
+
+        test.float.should eq 20.32f32
+
+        test.gtt.should eq true
+        test.gtg.should eq 20.0855369
+      end
+    end
+
+    it "encodes" do
+      File.open("#{__DIR__}/../fixtures/test.data.proto3-encoded") do |io|
+        test = TestMessagesV3::Test3.from_protobuf(io)
+        encoded = test.to_protobuf
+        test2 = TestMessagesV3::Test3.from_protobuf(encoded.rewind)
+        test.should eq(test2)
+      end
+    end
+  end
 end
